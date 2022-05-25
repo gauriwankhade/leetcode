a85acf4bc13d36507e966f553e13ffd9e7dbfc55
gauriwankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-05-25 19:11:39 +0530

    Time: 1009 ms (20.32%), Space: 14.6 MB (55.08%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def pathSum(self, root, targetSum):
+        stack = [root]
+        self.count = 0
+        self.targetSum = targetSum
+
+        while(stack):
+            top = stack.pop(0)
+            
+            if top:
+                stack.append(top.left)
+                stack.append(top.right)
+            self.traverseTree(top, 0)
+
+
+        return self.count
+
+
+
+    def traverseTree(self, curr, summ):
+        if not curr:
+            return
+
+        
+        summ += curr.val
+
+        if summ == self.targetSum:
+            self.count += 1
+
+        self.traverseTree(curr.left, summ)
+        self.traverseTree(curr.right, summ)
