2e83f1caaa969bb666ee6035847d09b9636241f6
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-05-26 13:00:41 +0530

    Time: 517 ms (21.58%), Space: 20.7 MB (57.26%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
@@ -9,10 +9,19 @@ class Solution(object):
         numLen = len(nums) 
         
         for index in range(numLen):
-            if nums[abs(nums[index]) - 1] < 0:
-            	ans.append(abs(nums[index]))
-            elif nums[abs(nums[index]) - 1] > 0:
-            	nums[abs(nums[index]) - 1] =  -1 * nums[abs(nums[index]) - 1] 
+            newInd = abs(nums[index]) - 1 
+            if nums[newInd] < 0:
+                ans.append(newInd + 1)
+            elif nums[newInd] > 0:
+            	nums[newInd] =  -1 * nums[newInd] 
         
         
         return ans
+    
+    
+
+        
+        
+        
+    
+    
