3bf54a60f2cb4c33a5ea5e0afa01806b4ab84eb9
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-05-30 00:33:36 +0530

    Time: 39 ms (15.68%), Space: 13.6 MB (46.44%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,31 +1,20 @@
 
 class Solution(object):
-	def helper(self, num, stack):
-		
-		for neighbor in range(num+1, self.length):
-			self.helper(neighbor, stack + [self.nums[neighbor]])
-
-		if len(stack) > 1:
-			self.result.append(stack)
-
-
-
 	def subsets(self, nums):
-		self.graph = {}
-		self.result = [[]]
-		self.length = len(nums)
-		self.nums = nums
-		# for i in range(len(nums)):
-		# 	for j in range(i + 1, len(nums)):
-		# 		if not self.graph.get(nums[i]):
-		# 			self.graph[nums[i]] = [nums[j]]
-		# 		else:
-		# 			self.graph[nums[i]].append(nums[j])
+		result = [[]]
+		length = len(nums)
+
+		def helper(num, stack):	
+			for neighbor in range(num+1, length):
+				helper(neighbor, stack + [nums[neighbor]])
+
+			if len(stack) > 1:
+				result.append(stack)
 
 
-		for num in range(self.length):
-			self.result.append([nums[num]])
-			self.helper(num, [nums[num]])
+		for num in range(length):
+			result.append([nums[num]])
+			helper(num, [nums[num]])
 
 
-		return self.result
+		return result
