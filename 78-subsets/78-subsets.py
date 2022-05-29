d4e1809fb23b594b3bf2ab7571e4f11a3fecc486
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-05-30 00:30:38 +0530

    Time: 44 ms (7.81%), Space: 13.4 MB (91.89%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,9 +1,9 @@
 
 class Solution(object):
 	def helper(self, num, stack):
-		if self.graph.get(num):
-			for neighbor in self.graph[num]:
-				self.helper(neighbor, stack + [neighbor])
+		
+		for neighbor in range(num+1, self.length):
+			self.helper(neighbor, stack + [self.nums[neighbor]])
 
 		if len(stack) > 1:
 			self.result.append(stack)
@@ -13,17 +13,19 @@ class Solution(object):
 	def subsets(self, nums):
 		self.graph = {}
 		self.result = [[]]
-		for i in range(len(nums)):
-			for j in range(i + 1, len(nums)):
-				if not self.graph.get(nums[i]):
-					self.graph[nums[i]] = [nums[j]]
-				else:
-					self.graph[nums[i]].append(nums[j])
+		self.length = len(nums)
+		self.nums = nums
+		# for i in range(len(nums)):
+		# 	for j in range(i + 1, len(nums)):
+		# 		if not self.graph.get(nums[i]):
+		# 			self.graph[nums[i]] = [nums[j]]
+		# 		else:
+		# 			self.graph[nums[i]].append(nums[j])
 
 
-		for num in nums:
-			self.result.append([num])
-			self.helper(num, [num])
+		for num in range(self.length):
+			self.result.append([nums[num]])
+			self.helper(num, [nums[num]])
 
 
 		return self.result
