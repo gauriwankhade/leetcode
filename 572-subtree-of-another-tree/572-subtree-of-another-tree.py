35ae84e606182e9bc2e0910a97e2d542159e3b26
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-06-09 20:18:46 +0530

    Time: 275 ms (6.61%), Space: 15.1 MB (61.78%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -7,7 +7,7 @@
 
 class Solution(object):
     def isSubtree(self, root, subRoot):
-        self.ans = False
+       
 
         def isSameTree(p, q):
             if not p and not q:
@@ -19,19 +19,19 @@ class Solution(object):
                 if isSameTree(p.left, q.left) and isSameTree(p.right, q.right):
                     return True
 
-        def findSubRoot(curr, sub):
+        def findSubRoot(curr, sub, ans):
             if not curr:
-                return 
+                return ans
 
            
-            findSubRoot(curr.left, sub)
-            findSubRoot(curr.right, sub)
+            ans = findSubRoot(curr.left, sub, ans)
+            ans = findSubRoot(curr.right, sub, ans)
             
             if curr.val == sub.val and isSameTree(curr, sub):
-                self.ans = True
+                ans = True
                 
-            return self.ans
+            return ans
 
-        return findSubRoot(root, subRoot)
+        return findSubRoot(root, subRoot, False)
 
         #return self.ans
