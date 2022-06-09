4a377d5109ae80c2faf7ee50665aa7b5b309a192
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-06-10 00:05:32 +0530

    Time: 49 ms (5.08%), Space: 13.4 MB (65.69%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -2,8 +2,6 @@ class Solution(object):
 	def getRow(self, rowIndex):
 		dp = [[0 for col in range(rowIndex + 1)] for row in range(rowIndex + 1)]
 
-		# for col in dp:
-		# 	col[0] = 1
 		dp[0][0] = 1
 		for i in range(1, len(dp)):
 			dp[i][0] = 1
