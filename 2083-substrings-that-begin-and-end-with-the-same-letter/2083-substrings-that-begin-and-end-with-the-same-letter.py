af936a36791792dea61d920f740443077a57ca4e
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-06-14 03:06:11 +0530

    Time: 157 ms (42.86%), Space: 15.2 MB (21.43%) - LeetHub

--- 2083-substrings-that-begin-and-end-with-the-same-letter/2083-substrings-that-begin-and-end-with-the-same-letter.py
+++ 2083-substrings-that-begin-and-end-with-the-same-letter/2083-substrings-that-begin-and-end-with-the-same-letter.py
@@ -1,18 +1,13 @@
 class Solution(object):
    
     def numberOfSubstrings(self, s):
-        Map = defaultdict(list)
-        ans = 0
+        Map = Counter(s)
+        ans = len(s)
         
-        for key in s:
-            if not Map[key]:
-                Map[key] = [1, 1]
-            else:
-                Map[key][0] += 1
-                Map[key][1] = Map[key][0] + Map[key][1] 
-            
-        
-        return sum([Map[i][1] for i in Map])
+        for key in Map:
+            ans += ((Map[key] * (Map[key] - 1)) / 2)
+                 
+        return ans
                 
             
         
