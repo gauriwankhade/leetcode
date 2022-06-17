abf21151772dc759a20a155d755f890113eaf2ad
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-06-17 15:19:05 +0530

    Time: 21 ms (99.64%), Space: 13.7 MB (89.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,15 +1,13 @@
 class Solution(object):
     def isAnagram(self, s, t):
         alpha = 'abcdefghijklmnopqrstuvwxyz'
-        s = sorted(s)
-        t = sorted(t)
         length = len(s)
         
         if length != len(t):
             return
         
-        for idx in range(length):
-            if s[idx] != t[idx]:
+        for char in alpha:
+            if s.count(char) != t.count(char):
                 return False
         
         return True
