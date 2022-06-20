eb3840f16becdca26e000decc1aa89b05477ec74
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-06-20 15:23:26 +0530

    Time: 133 ms (43.16%), Space: 19.5 MB (25.83%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -3,12 +3,11 @@ from collections import defaultdict
 class Solution(object):
 	def groupAnagrams(self, strs):
 		ans = collections.defaultdict(list)
-		alpha = 'abcdefghijklmnopqrstuvwxyz'
 
 		for item in strs:
 			chars = [0] * 26
 			for char in item:
-				chars[alpha.find(char)] += 1
+				chars[ord(char) - ord('a')] += 1
 
 			ans[tuple(chars)].append(item)
 
