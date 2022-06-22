65a9e823b562477226af88fa851030e7cc884d6c
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-06-22 17:06:52 +0530

    Time: 71 ms (67.05%), Space: 14.3 MB (68.22%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -3,7 +3,7 @@ class Solution(object):
         Map = {}
         
         for num in range(len(nums)):    
-            if target - nums[num] in Map and Map[target - nums[num]] != num:
+            if target - nums[num] in Map:
                 return [num,  Map[target - nums[num]]]
             
             Map[nums[num]] = num
