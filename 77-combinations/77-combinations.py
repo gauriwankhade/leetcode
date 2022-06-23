93988c23caffbda6287b264974c44eb500f0ce47
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-06-23 18:58:25 +0530

    Time: 720 ms (25.01%), Space: 16 MB (50.08%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,11 +1,9 @@
 class Solution(object):
 	## Aproach 1 - Backtracking
 	def combine(self, n, k):
-		# loop over all elements - range(1, n - k + 1)
-		# in each iteration find combinations includes - current and length is k
-
 		result = []
 		arr = []
+        
 		def backtrack(curr, limit, n):
 			if len(arr) == limit:
 				result.append(arr[0: ])
@@ -17,7 +15,6 @@ class Solution(object):
 				arr.pop()
 
 			return result
-
 
 		return backtrack(1, k, n + 1)
 
