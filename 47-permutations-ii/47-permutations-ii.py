a641e8c5fcfd39c69fde8d43082d3220f73e63c3
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-06-23 21:55:43 +0530

    Time: 434 ms (23.95%), Space: 14.2 MB (7.90%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,21 +1,20 @@
 class Solution(object):
 	def permuteUnique(self, nums):
 		result = defaultdict(list)
-		limit = len(nums)
 		visited = defaultdict(bool)
 
-		def backtrack(curr, arr, limit):
-			if len(arr) == limit:
+		def backtrack(curr, arr):
+			if len(arr) == len(nums):
 				result[tuple(arr)] = arr
 				return 
 
-			for i in range(limit):
+			for i in range(len(nums)):
 				if not visited[i]:
 					visited[i] = True
-					backtrack(i + 1, arr + [nums[i]], limit)
+					backtrack(i + 1, arr + [nums[i]])
 					visited[i] = False
 
 			return result
 
-		return backtrack(0, [], limit).values()
+		return backtrack(0, []).values()
 		
