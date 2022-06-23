4721e9d3c51509fa01fd3ff35144a03f31b27e70
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-06-23 15:26:04 +0530

    Time: 714 ms (31.03%), Space: 14.8 MB (84.01%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    
+    def combine(self, n, k):
+		# loop over all elements - range(1, n - k + 1)
+		# in each iteration find combinations includes - current and length is k
+
+		result = []
+		def backtrack(curr, arr, limit, n):
+			if len(arr) == limit:
+				result.append(arr)
+				return
+
+			for i in range(curr + 1, n):
+				backtrack(i, arr + [i], limit, n)
+
+
+		for num in range(1, n - k + 2):
+			backtrack(num, [num], k, n + 1)
+
+
+		return result
+
+
+        
+       
