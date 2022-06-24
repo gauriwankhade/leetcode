b5e3687471a8315690ef92002c399fa936cda347
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-06-24 22:44:03 +0530

    Time: 32 ms (50.28%), Space: 13.8 MB (39.22%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,20 +1,19 @@
 class Solution(object):
 	def generateParenthesis(self, n):
-		string = '()' * n
 		result = set()
-		count = {1: '(', 0: ')'}
 
-		def backtrack(curr, comb, openCount, closedCount):
-			if max(openCount, closedCount) > len(string)/2:
+		def backtrack(curr, comb, openCount, closedCount, n):
+			if max(openCount, closedCount) > n:
 				return
 			if openCount >= closedCount:
-				if len(comb) >= len(string):
+				if openCount + closedCount >= 2 * n:
 					result.add(comb)
 					return
 
-				backtrack(0, comb + count[0], openCount, closedCount + 1)
-				backtrack(1, comb + count[1], openCount + 1, closedCount)			
+				backtrack(0, comb + '(', openCount + 1, closedCount, n)
+				backtrack(1, comb + ')', openCount, closedCount + 1, n)			
 
-		backtrack(0, "", 0, 0)
+		backtrack(0, "", 0, 0, n)
 
 		return result
+
