f77afec548799ecbd7a4a4fd3a240fe19b43d387
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-06-24 22:47:01 +0530

    Time: 33 ms (46.87%), Space: 13.7 MB (39.22%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -3,18 +3,15 @@ class Solution(object):
 		result = set()
 
 		def backtrack(curr, comb, openCount, closedCount, n):
-			if max(openCount, closedCount) > n:
-				return
 			if openCount >= closedCount:
 				if openCount + closedCount >= 2 * n:
 					result.add(comb)
 					return
-
-				backtrack(0, comb + '(', openCount + 1, closedCount, n)
-				backtrack(1, comb + ')', openCount, closedCount + 1, n)	
+				if openCount < n:
+					backtrack(0, comb + '(', openCount + 1, closedCount, n)
+				if closedCount < n:
+					backtrack(1, comb + ')', openCount, closedCount + 1, n)	
 
 			return result		
 
 		return backtrack(0, "", 0, 0, n)
-
-
