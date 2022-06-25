28c6169be189bcd1ba042bfad71451193db3f0b8
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-06-25 15:32:19 +0530

    Time: 134 ms (27.60%), Space: 15.4 MB (5.91%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -0,0 +1,22 @@
+class Solution(object):
+	def allPathsSourceTarget(self, graph):
+		visited = defaultdict(int)
+		result = set()
+
+		def backtrack(curr, path):
+			if curr == len(graph) - 1:
+				result.add(tuple(path))
+				return
+			if not graph[curr]:
+				return
+
+			for neighbour in graph[curr]:
+				if not visited[neighbour]:
+					visited[neighbour] = True
+					backtrack(neighbour, path + [neighbour])
+					visited[neighbour] = False
+
+			return result
+
+
+		return backtrack(0, [0])
