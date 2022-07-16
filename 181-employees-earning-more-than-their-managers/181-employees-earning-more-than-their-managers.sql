b37d3a2b7641bbb6f562b7395294b2a849e9fb57
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-07-16 20:22:59 +0530

    Time: 320 ms (92.34%), Space: 0B (100.00%) - LeetHub

--- 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -6,13 +6,18 @@
 
 
 
-SELECT
-    e.name AS Employee
-
-FROM
-    Employee AS e,
-    Employee AS m
-WHERE
-    e.ManagerId = m.id AND
-    e.salary > m.salary;
+# SELECT
+#     e.name AS Employee
+# FROM
+#     Employee AS e,
+#     Employee AS m
+# WHERE
+#     e.ManagerId = m.id AND
+#     e.salary > m.salary;
     
+SELECT
+     a.NAME AS Employee
+FROM Employee AS a JOIN Employee AS b
+     ON a.ManagerId = b.Id
+     AND a.Salary > b.Salary
+;
