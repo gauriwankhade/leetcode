66303b616fed1914e84d4b77ee7095c9c3a76f09
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-07-17 03:18:35 +0530

    Time: 51 ms (37.93%), Space: 13.4 MB (91.95%) - LeetHub

---
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def secondHighest(self, s):
+        
+        firstMax, secondMax = float('-inf'), -1
+        
+        for char in s:
+            if char.isnumeric():
+                if firstMax < int(char):
+                    firstMax, secondMax = int(char), firstMax
+                elif firstMax != int(char):
+                    secondMax = max(secondMax, int(char))
+        print(firstMax, secondMax)   
+        return secondMax if secondMax not in [float('-inf'), firstMax] else -1
