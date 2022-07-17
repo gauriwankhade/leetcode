5c171767b8028ff8ce992836bd4334cbbd928373
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2022-07-18 00:50:55 +0530

    Time: 170 ms (92.04%), Space: 14.7 MB (45.58%) - LeetHub

---
+++ 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def repeatedNTimes(self, nums):
+        start = 0
+        end = len(nums) - 1
+        
+        while(start < end):
+            
+            if start + 1 < end and nums[start + 1] in [nums[start], nums[end]]:
+                return nums[start + 1]
+            if end - 1 > start and nums[end - 1] in [nums[start], nums[end]]:
+                return nums[end - 1]
+            if nums[start] == nums[end]:
+                return nums[start]
+            
+            start += 1
+            end -= 1
+            
+            
+            
+        
